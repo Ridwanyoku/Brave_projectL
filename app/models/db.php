62e3648721db60ecb6f87e35e6557042940b87c7
AA-07 <firmansyahaziz38@gmail.com>
2024-12-04 11:45:41 +0700

    connect db

--- app/models/db.php
+++ app/models/db.php
@@ -1,5 +1,21 @@
 <?php
 
+$host = 'localhost';
+$db = 'brave';
+$user = 'root';  
+$pass = '';
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database: " . $e->getMessage());
+}
 
 
 ?>
